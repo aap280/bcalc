4785c24bd2b5e7ffa99cc336a3ee71b842ba7699
Arthur <aap280@gmail.com>
2017-01-22 18:24:58 -0600

    initial file add

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bcalcApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bcalcApp
+ */
+angular.module('bcalcApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bcalcApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bcalcApp
+ */
+angular.module('bcalcApp')
+  .controller('MainCtrl', function () {
+    //$scope.calculateBurn(billableHrs, daysPassed, totalMonthDays) {	//rewrite this garbage
+    //	return ((billableHrs / daysPassed) * totalMonthDays);
+    //}
+    function calculateBurn(){
+    	alert('aye');
+    }
+  });
